1f3ef40420e5225e17ed8ee9099069473b250de6
Rafael Bolaños <rbolanoherrera@gmail.com>
2019-04-09 08:48:54 -0500

    Cargar tabla con .csv
    Cargar una tabla en Sql Server con el contenido de un archivo con el metodo BULK INSERT

---
+++ BulkTblPrueba.sql
@@ -0,0 +1,9 @@
+BULK INSERT tbl_prueba
+FROM 'd:\Data\testFile.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
